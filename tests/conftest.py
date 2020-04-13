9593b61d79d8de5a3f65eb870ad115405237eba8
ocni <ocni@dtu.dk>
2020-04-13 09:41:07 +0200

    updates tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,9 @@
 __author__ = "Christian Kongsgaard"
 __license__ = 'MIT'
 
+import pytest
+
+
+@pytest.fixture
+def repo_updates():
+    pass
--- tests/test_git.py
+++ tests/test_git.py
@@ -2,18 +2,24 @@ __author__ = "Christian Kongsgaard"
 __license__ = 'MIT'
 
 
+def test_pull_repo():
+    """
+    Pull repo
+    Pull repo without password
+    Pull repo with password and username
+    Pull repo without repo
+    """
+    pass
 
-"""
-TESTS:
 
-Pull repo
-Pull repo without password
-Pull repo with password and username
-Pull repo without repo
+def test_check_for_updates(repo_updates):
+    """
+    Check repo with updates
+    Check repo without updates
+    Check repo with updates but not to stacks
+    """
 
+    path = repo_updates
+    updates = check_for_updates(path)
 
-Check repo with updates
-Check repo without updates
-Check repo with updates but not to stacks
-
-"""
+    assert updates
